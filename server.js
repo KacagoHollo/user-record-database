e5a2793c81f4a57873cba07bbe3f338f09ac9bf3
IstvanPongracz <pongracz88@gmail.com>
2022-04-22 23:40:58 +0200

    map data, new user

--- server.js
+++ server.js
@@ -12,19 +12,49 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-mongoose
-.connect(process.env.USERS)
-.then(() => console.log("Connected"))
-.catch(() => console.log("Error"));
 
 // mongoose.connection.db.dropDatabase();
 
-app.get("/", (req, res) => {
-   const response = http.get('https://jsonplaceholder.typicode.com/users');
-    res.send(response.data)
+const loadFunction = async () => {
+  mongoose
+  .connect(process.env.USERS)
+  .then(() => console.log("Connected"))
+  .catch(() => console.log("Error"));
 
+  const response = await http.get('https://jsonplaceholder.typicode.com/users');
+  const userData = response.data
+
+  userData.map((person) => {
     
+    const newUser = new User ({
+      id: person.id,
+        name: person.name,
+        username: person.username,
+        email: person.email,
+        address: {street: person.street, suite: person.suite, city: person.city, zipcode: person.zipcode, geo: {
+          lat: person.lat, lng: person.lng  
+        }},
+        phone: person.phone,
+        website: person.website,
+        company: {
+            name: person.name,
+            catchPhrase: person.catchPhrase,
+            bs: person.bs
+        } 
+    })
+    newUser.save((err) => {
+      if (err) console.log(err)
+    } 
+     )
+  }
+  )
 
+}
+
+
+
+
+app.get("/", (req, res) => {
     const userSchema = new mongoose.Schema({
         id: Number,
         name: String,
