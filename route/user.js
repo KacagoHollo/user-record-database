e87b7a2d8b7b4d258ec8ab271df43f6817e6cf12
IstvanPongracz <pongracz88@gmail.com>
2022-04-25 15:50:27 +0200

    Created endpoints for queries (id, username)

--- route/user.js
+++ route/user.js
@@ -5,12 +5,19 @@ router.get('/', async (req, res) => {
   let users = await User.find({name: new RegExp(req.query.name, "i")})
     .select(req.query.all ? null : 'name')
     .limit(req.query.limit)
-    .sort(req.query.order_by ? 'name' : null)
+    .sort(req.query.order_by ? req.query.order_by : undefined)
   res.json(users)
 })
-  // })
 
+router.get('/id/:id', async (req, res) => {
+  let users = await User.find({id: Number(req.params.id, "i")})
+  res.json(users)
+})
 
+router.get('/username/:username', async (req, res) => {
+  let users = await User.find({username: new RegExp(`^${req.params.username}`, "i")})
+  res.json(users)
+})
 
 
 
